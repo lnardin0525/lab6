e3d7f5c2eaf6ba90716f8b199cee810f4f05bb3c
Lucas Nardin <lnardin@ufl.edu>
2023-07-19 22:22:55 -0400

    minor fixes

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def encode(password):
 def decode(encoded_password):
     decoded_list = []
     for char in encoded_password:
-        decoded_char = str(int(char) + 3)
+        decoded_char = str(int(char) - 3)
         if int(decoded_char) < 0:
             decoded_char = str(int(decoded_char) + 10)
         decoded_list.append(decoded_char)
@@ -34,12 +34,13 @@ if __name__ == "__main__":
         user_input = input('Please enter an option: ')
 
         if user_input == '1':
-            password = encode(input('Please enter your password to encode: '))
+            encoded_password = encode(input('Please enter your password to encode: '))
             print(f'Your password has been encoded and stored!\n')
 
         elif user_input == '2':
-            decoded_password = decode(encoded_password=[])
+            decoded_password = decode(encoded_password)
             print(f"The encoded password is {encoded_password}, and the original password is {decoded_password}.\n")
+
         elif user_input == '3':
             program_quit = True
             break
